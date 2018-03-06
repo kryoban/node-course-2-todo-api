e46a4903f90413a89f90b9942bab0dc6ed8891a6
Toma Popescu <Toma.Popescu@endava.com>
2018-03-06 15:17:11 +0200

    Added GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,24 +32,56 @@ var UserSchema = new mongoose.Schema({
     }]
 })
 
-UserSchema.methods.toJSON = function () {
-    var user = this;
-    var userObject = user.toObject();
+UserSchema.methods = {
+    toJSON: function () {
+        var user = this;
+        var userObject = user.toObject();
 
-    return _.pick(userObject, ['_id', 'email']);
+        return _.pick(userObject, ['_id', 'email']);
+    },
+    generateAuthToken: function () {
+        var user = this;
+        var access = 'auth';
+        var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+
+        user.tokens = user.tokens.concat([{ access, token }]);
+
+        return user.save().then(() => {
+            return token
+        })
+    }
 }
 
-UserSchema.methods.generateAuthToken = function () {
-    var user = this;
-    var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+UserSchema.statics = {
+    findByToken: function (token) {
+        var User = this;
+        var decoded;
 
-    user.tokens = user.tokens.concat([{ access, token }]);
+        try {
+            decoded = jwt.verify(token, 'abc123');
+        } catch (e) {
+            return Promise.reject();
+        }
 
-    return user.save().then(() => {
-        return token
-    })
+        return User.findOne({
+            _id: decoded._id,
+            'tokens.token': token,
+            'tokens.access': 'auth'
+        });
+    }
 }
+
+// UserSchema.methods.generateAuthToken = function () {
+//     var user = this;
+//     var access = 'auth';
+//     var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+
+//     user.tokens = user.tokens.concat([{ access, token }]);
+
+//     return user.save().then(() => {
+//         return token
+//     })
+// }
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -105,6 +106,19 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
 })
 
 
