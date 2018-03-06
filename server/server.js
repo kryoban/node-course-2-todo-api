27911ac8bd4aba2fd60885d0b89d49b151c154ec
Toma Popescu <Toma.Popescu@endava.com>
2018-03-06 12:47:48 +0200

    Added token method

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send({message: 'Welcome!'});
+    res.send({ message: 'Welcome!' });
 })
 
 //////// TODOS ////////
@@ -32,7 +32,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     })
@@ -41,14 +41,14 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
-        return res.status(400).send({error: 'Todo ID is invalid'});   
+        return res.status(400).send({ error: 'Todo ID is invalid' });
     }
-    
+
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            return res.status(404).send({error: 'Todo not found'});
+            return res.status(404).send({ error: 'Todo not found' });
         }
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         res.status(400).send();
     })
@@ -57,22 +57,22 @@ app.get('/todos/:id', (req, res) => {
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
-        return res.status(400).send({error: 'Todo ID is invalid'});
-    } 
+        return res.status(400).send({ error: 'Todo ID is invalid' });
+    }
     Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
-            return res.status(404).send({error: 'Todo not found'});
+            return res.status(404).send({ error: 'Todo not found' });
         }
-        return res.send({todo});
+        return res.send({ todo });
     }).catch((e) => {
-        return res.status(400).send({error: e});
+        return res.status(400).send({ error: e });
     })
 })
 
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
- 
+
     if (!ObjectID.isValid(id)) {
         return res.status(400).send();
     }
@@ -83,11 +83,11 @@ app.patch('/todos/:id', (req, res) => {
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
-        res.send({todo});
+        res.send({ todo });
     }).catch((err) => {
         res.status(400).send();
     })
@@ -98,16 +98,18 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save().then((user) => {
-        res.send(user);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     })
 })
-    
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
 
-module.exports = {app};
+module.exports = { app };
