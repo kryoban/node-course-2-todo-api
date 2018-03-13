b2f694d43f246ccf11ffbdea12982911c0ae0909
tom <popescut@gmail.com>
2018-03-13 12:53:35 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,11 @@
 // sau este setat in package.json in scriptul de "test"
 // daca rulam aplicatia local, setam automat mediul (process.env.NODE_ENV) ca fiind "development"
 var env = process.env.NODE_ENV || 'development';
-console.log('env ******', env);
 
-// o data stabilit environmentul setam portul pe care va rula serverul express si calea catre baza de date
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json')[env];
+    
+    Object.keys(config).forEach((key) => {
+        process.env[key] = config[key];
+    })
 }
